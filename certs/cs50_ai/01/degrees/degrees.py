9d294acc58d131625b40160522f70a627d7a0b33
bealljames <bealljamesp@proton.me>
2024-02-02 22:32:05 -0800

    202402021232

--- certs/cs50_ai/01/degrees/degrees.py
+++ certs/cs50_ai/01/degrees/degrees.py
@@ -92,7 +92,49 @@ def shortest_path(source, target):
     If no possible path, returns None.
     """
 
-    # TODO
+    # Initialize frontier to just the starting position
+    start = Node(state=source, parent=None, action=None)
+    frontier = QueueFrontier()
+    frontier.add(start)
+
+    # Initialize an empty explored set
+    explored = set()
+
+    # Keep looping until solution found
+    while True:
+
+        # If nothing left in frontier, then no path
+        if frontier.empty():
+            return None
+
+        # Choose a node from the frontier
+        node = frontier.remove()
+        state = node.state
+
+        # If node is the goal, then we have a solution
+        if state == target:
+            actions = []
+            cells = []
+            while node.parent is not None:
+                actions.append(node.action)
+                cells.append(node.state)
+                node = node.parent
+            actions.reverse()
+            cells.reverse()
+            solution = list(zip(actions, cells))
+            return solution
+
+        # Mark node as explored
+        explored.add(state)
+
+        # Add neighbors to frontier
+        for action, state in neighbors_for_person(state):
+            if not frontier.contains_state(state) and state not in explored:
+                child = Node(state=state, parent=node, action=action)
+                frontier.add(child)
+                
+    # If no solution, then no path
+
     raise NotImplementedError
 
 
