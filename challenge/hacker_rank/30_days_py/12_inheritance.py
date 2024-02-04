4e14efe362d95bb5071d230f99c973c1c937aa84
bealljames <bealljamesp@proton.me>
2024-02-04 14:37:08 -0800

    hacker_rank

--- challenge/hacker_rank/30_days_py/12_inheritance.py
+++ challenge/hacker_rank/30_days_py/12_inheritance.py
@@ -1,32 +1,19 @@
 class Person:
-    def __init__(self, first_name, last_name, id_number):
-        self.first_name = first_name
-        self.last_name = last_name
-        self.id_number = id_number
-    def print_person(self):
-        '''Print Student Name and ID'''
-        print("Name:", self.last_name + ",", self.first_name)
-        print("ID:", self.id_number)
+	def __init__(self, firstName, lastName, idNumber):
+		self.firstName = firstName
+		self.lastName = lastName
+		self.idNumber = idNumber
+	def printPerson(self):
+		print("Name:", self.lastName + ",", self.firstName)
+		print("ID:", self.idNumber)
 
 class Student(Person):
-    '''Class Constructor'''
-    # Parameters:
-    # first_name - A string denoting the Person's first name.
-    # last_name - A string denoting the Person's last name.
-    # id - An integer denoting the Person's ID number.
-    # scores - An array of integers denoting the Person's test scores.
-
-    # Write your constructor here
+    
     def __init__(self, first_name, last_name, id_number, scores):
         self.scores = scores
         super().__init__(first_name, last_name, id_number)
 
-    # Function Name: calculate
-    # Return: A character denoting the grade.
-
-    # Write your function here
     def calculate(self):
-        '''Calcuate Scores'''
 
         average = sum(self.scores) / len(self.scores)
         if 90 <= average <= 100:
@@ -42,15 +29,20 @@ class Student(Person):
         else:
             return 'T'
 
-LINE = input().split()
-FIRST_NAME = LINE[0]
-LAST_NAME = LINE[1]
-ID_NUMBER = LINE[2]
+line = input().split()
+firstName = line[0]
+lastName = line[1]
+idNum = line[2]
+numScores = int(input()) # not needed for Python
+scores = list( map(int, input().split()) )
+s = Student(firstName, lastName, idNum, scores)
+s.printPerson()
+print("Grade:", s.calculate())
 
-NUM_SCORES = int(input()) # not needed for Python
-SCORES = list( map(int, input().split()))
+# Heraldo Memelli 8135627
+# 2
+# 100 80
 
-S = Student(FIRST_NAME, LAST_NAME, ID_NUMBER, SCORES)
-S.print_person()
-
-print("Grade:", S.calculate())
+# Name: Memelli, Heraldo
+# ID: 8135627
+# Grade: O
