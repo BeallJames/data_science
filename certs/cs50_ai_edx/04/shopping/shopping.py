3b7a450fb75e2a44ec4d469e5bccdad573d44535
James <119552925+BeallJames@users.noreply.github.com>
2024-03-08 16:17:57 -0800

    updates

--- certs/cs50_ai_edx/04/shopping/shopping.py
+++ certs/cs50_ai_edx/04/shopping/shopping.py
@@ -6,6 +6,7 @@ from sklearn.neighbors import KNeighborsClassifier
 
 TEST_SIZE = 0.4
 
+k = 1
 
 def main():
 
@@ -59,7 +60,62 @@ def load_data(filename):
     labels should be the corresponding list of labels, where each label
     is 1 if Revenue is true, and 0 otherwise.
     """
-    raise NotImplementedError
+    # Dictionary Mapping Months to Numerical values
+    months = {'Jan': 0, 'Feb': 1, 'Mar': 2, 'Apr': 3, 'May': 4, 'June': 5, 'Jul': 6, 'Aug': 7, 'Sep': 8, 'Oct': 9, 'Nov': 10, 'Dec': 11}
+
+    # Mapping Visitor Types to integers
+    visitors = {'Returning_Visitor': 1, 'New_Visitor': 0, 'Other': 0}
+
+    # Mapping Boolean Strings to integers
+    bools = {'TRUE': 1, 'FALSE': 0}
+
+    # Create list of lists for evidence, list for labels:
+    evidence = []
+    labels = []
+
+    # Open CSV file and load in data as dict:
+    with open(filename, newline='') as csvfile:
+        csvreader = csv.DictReader(csvfile, delimiter=',')
+        print('Loading Data from csv file...')
+        lines = 0
+        for row in csvreader:
+
+            lines += 1
+
+            line = []
+
+            # Append Evidence to List of Lists
+            line.append(int(row['Administrative']))
+            line.append(float(row['Administrative_Duration']))
+            line.append(int(row['Informational']))
+            line.append(float(row['Informational_Duration']))
+            line.append(int(row['ProductRelated']))
+            line.append(float(row['ProductRelated_Duration']))
+            line.append(float(row['BounceRates']))
+            line.append(float(row['ExitRates']))
+            line.append(float(row['PageValues']))
+            line.append(float(row['SpecialDay']))
+            line.append(months[row['Month']])
+            line.append(int(row['OperatingSystems']))
+            line.append(int(row['Browser']))
+            line.append(int(row['Region']))
+            line.append(int(row['TrafficType']))
+            line.append(visitors[row['VisitorType']])
+            line.append(bools[row['Weekend']])
+
+            # Add evidence line to evidence
+            evidence.append(line)
+
+            # Append Labels to List
+            labels.append(bools[row['Revenue']])
+
+        # Confirm data loaded in successfully:
+        if len(evidence) != len(labels):
+            sys.exit('Error when loading data! Evidence length does not match label length')
+
+        print('Data loaded successfully from csv file! Total lines: ', lines)
+
+        return evidence, labels
 
 
 def train_model(evidence, labels):
@@ -67,7 +123,12 @@ def train_model(evidence, labels):
     Given a list of evidence lists and a list of labels, return a
     fitted k-nearest neighbor model (k=1) trained on the data.
     """
-    raise NotImplementedError
+    print('Fitting Model using k-Nearest Neighbours Classifier, with k = ', k)
+
+    model = KNeighborsClassifier(n_neighbors=k)
+    model.fit(evidence, labels)
+
+    return model
 
 
 def evaluate(labels, predictions):
@@ -85,7 +146,23 @@ def evaluate(labels, predictions):
     representing the "true negative rate": the proportion of
     actual negative labels that were accurately identified.
     """
-    raise NotImplementedError
+    pos_labels = labels.count(1)
+    neg_labels = labels.count(0)
+
+    correct_positive = 0
+    correct_negative = 0
+
+    for i in range(len(predictions)):
+        if predictions[i] == labels[i]:
+            if predictions[i] == 1:
+                correct_positive += 1
+            else:
+                correct_negative += 1
+
+    sensitivity = correct_positive / pos_labels
+    specificity = correct_negative / neg_labels
+
+    return sensitivity, specificity
 
 
 if __name__ == "__main__":
