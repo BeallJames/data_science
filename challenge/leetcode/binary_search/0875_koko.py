29815ab383ffa9948435d09af3261002b01d6105
James <119552925+BeallJames@users.noreply.github.com>
2024-03-16 14:00:15 -0700

    Create 0875_koko.py

---
+++ challenge/leetcode/binary_search/0875_koko.py
@@ -0,0 +1,13 @@
+from typing import List
+
+
+class Solution:
+    def minEatingSpeed(self, piles: List[int], h: int) -> int:
+        return h
+
+
+piles = [3, 6, 7, 11]
+h = 8
+
+sol = Solution()
+print(sol.minEatingSpeed(piles, h))
