3507754ac7308bf00387fbb939e6c45ec6f44d0f
James <119552925+BeallJames@users.noreply.github.com>
2024-03-17 20:48:52 -0700

    Update alien_invasion.py

--- text/python/matthis_pycc/proejct_1/alien_invasion/alien_invasion.py
+++ text/python/matthis_pycc/proejct_1/alien_invasion/alien_invasion.py
@@ -27,6 +27,7 @@ class AlienInvasion:
         """Start with the main loop"""
         while True:
             self._check_events()
+            self.ship.update()
             self._update_screen()
 
     def _update_screen(self):
@@ -44,7 +45,10 @@ class AlienInvasion:
             elif event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_RIGHT:
                     # Move the ship right
-                    self.ship.rect.x += 1
+                    self.ship.moving_right = True
+            elif event.type == pygame.KEYUP:
+                if event.key == pygame.K_RIGHT:
+                    self.ship.moving_right = False
 
 
 if __name__ == "__main__":
