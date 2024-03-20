844a43234a5aadeb51b197f200008dbd23deeceb
James <119552925+BeallJames@users.noreply.github.com>
2024-03-20 11:29:44 -0700

    alien invasion

---
+++ text/python/matthis_pycc/proejct_1/alien_invasion/scoreboard.py
@@ -0,0 +1,35 @@
+import pygame.font
+
+
+class Scoreboard:
+    """scoreboard class"""
+
+    def __init__(self, ai_game):
+        """initialize scorekeeping attributes"""
+        self.screen = ai_game.screen
+        self.screen_rect = self.screen.get_rect()
+        self.settings = ai_game.settings
+        self.stats = ai_game.stats
+
+        # font settings for score info
+        self.text_color = (30, 30, 30)
+        self.font = pygame.Font.SysFont(None, 48)
+
+        # prepare init score image
+        self.prep_score()
+
+    def prep_score(self):
+        """turn the score into a rendered image"""
+        score_str = str(self.stats.score)
+        self.score_image = self.font.render(
+            score_str, True, self.text_color, self.settings.bg_color
+        )
+
+        # display score at top right
+        self.score_rect = self.score_image.get_rect()
+        self.score_rect.right = self.screen_rect.right - 20
+        self.score_rect.top = 20
+
+    def show_score(self):
+        """draw score to screen"""
+        self.screen.blit(self.score_image, self.score_rect)
