7f68c4f349c3cf0092630081c7787d2cded22c6d
James <119552925+BeallJames@users.noreply.github.com>
2024-03-20 17:17:11 -0700

    Update scoreboard.py

--- text/python/matthis_pycc/proejct_1/alien_invasion/scoreboard.py
+++ text/python/matthis_pycc/proejct_1/alien_invasion/scoreboard.py
@@ -49,3 +49,10 @@ class Scoreboard:
     def show_score(self):
         """draw score to screen"""
         self.screen.blit(self.score_image, self.score_rect)
+        self.screen.blit(self.high_score_image, self.high_score_rect)
+
+    def check_high_score(self):
+        """check for new high score"""
+        if self.stats.score > self.stats.high_score:
+            self.stats.high_score = self.stats.score
+            self.prep_high_score()
