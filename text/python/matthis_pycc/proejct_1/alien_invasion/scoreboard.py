2bdbe3dbf82a8150bd97ef503a83954632289c2d
James <119552925+BeallJames@users.noreply.github.com>
2024-03-20 17:11:30 -0700

    alien invasion

--- text/python/matthis_pycc/proejct_1/alien_invasion/scoreboard.py
+++ text/python/matthis_pycc/proejct_1/alien_invasion/scoreboard.py
@@ -15,8 +15,9 @@ class Scoreboard:
         self.text_color = (30, 30, 30)
         self.font = pygame.font.SysFont(None, 48)
 
-        # prepare init score image
+        # prepare init score images
         self.prep_score()
+        self.prep_high_score()
 
     def prep_score(self):
         """turn the score into a rendered image"""
@@ -31,6 +32,17 @@ class Scoreboard:
         self.score_rect = self.score_image.get_rect()
         self.score_rect.right = self.screen_rect.right - 20
         self.score_rect.top = 20
+
+    def high_score(self):
+        """turn high score into rendered image"""
+        high_score = round(self.stats.high_score. -1)
+        high_score_str = "{:,}".format(high_score)
+        self.high_score_image = self.font.render(high_score_str, True, self.text_color, self.settings.bg_color)
+
+        # center high score at top of screen 
+        self.high_score_rect = self.high_score_image.get_rect()
+        self.high_score_rect.centerx = self.screen_rect.centerx
+        self.high_score_rect.top = self.score_rect.top
 
     def show_score(self):
         """draw score to screen"""
