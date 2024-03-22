f78cfd2b26e6bf269a1c11dca6174a82b1de3088
James <119552925+BeallJames@users.noreply.github.com>
2024-03-22 11:17:12 -0700

    Update 0033_searchrotatedarray.py

--- challenge/leetcode/binary_search/0033_searchrotatedarray.py
+++ challenge/leetcode/binary_search/0033_searchrotatedarray.py
@@ -3,14 +3,19 @@ from typing import List
 
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
-        if target in nums:
+        # if target in nums:
+        #     return nums.index(target)
+        # else:
+        #     return -1
+
+        try:
             return nums.index(target)
-        else:
+        except:
             return -1
 
 
 nums = [4, 5, 6, 7, 0, 1, 2]
-target = 3
+target = 0
 # output 4
 
 s = Solution()
