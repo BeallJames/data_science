1a359c5375902c9b3cff91f3d8cb647596661beb
James <119552925+BeallJames@users.noreply.github.com>
2024-03-22 01:31:29 -0700

    lc 0875 koko

--- challenge/leetcode/binary_search/0875_koko.py
+++ challenge/leetcode/binary_search/0875_koko.py
@@ -3,23 +3,34 @@ from typing import List
 
 class Solution:
     def minEatingSpeed(self, piles: List[int], h: int) -> int:
-        for trysize in range(1, max(piles) + 1):
-            time = 0
-            for pile in piles:
-                if pile % trysize != 0:
-                    time += 1
-                    # print(f"%{pile} {trysize} {pile % trysize}")
-                time += pile // trysize
-                # print(f"//{pile} {trysize} {pile // trysize}")
-                # print(f"time {time}")
-            if time <= h:
-                smallestpile = trysize
-                break
-        return smallestpile
+        if len(piles) == 1:
+            if piles[0] % h != 0:
+                extra = 1
+            else:
+                extra = 0
+            return piles[0] // h + extra
+        else:
+            l, r = 1, max(piles)
+            pilesize = 0
+            while l <= r:
+                trysize = ((r - l) // 2) + l
+                print(f"{l} {r}")
+                time = 0
+                for pile in piles:
+                    if pile % trysize != 0:
+                        time += 1
+                    time += pile // trysize
+                print(f"trysize {trysize} time {time}")
+                if time <= h:
+                    pilesize = trysize
+                    r = trysize - 1
+                else:
+                    l = trysize + 1
+            return pilesize
 
 
-piles = [30, 11, 23, 4, 20]
-h = 5
+piles = [1000000000]
+h = 2
 
 sol = Solution()
 print(sol.minEatingSpeed(piles, h))
