0a8421b6f80af3840e55429cb555f575aa069a40
James <119552925+BeallJames@users.noreply.github.com>
2024-03-21 21:14:19 -0700

    Update rw_visual.py

--- text/python/matthis_pycc/project_2/generating_data/random_walk/rw_visual.py
+++ text/python/matthis_pycc/project_2/generating_data/random_walk/rw_visual.py
@@ -2,13 +2,26 @@ import matplotlib.pyplot as plt
 
 from random_walk import Randomwalk
 
+# keep making new walks
+while True:
+    # make a random walk
+    rw = Randomwalk()
+    rw.fill_walk()
 
-# make a random walk
-rw = Randomwalk()
-rw.fill_walk()
+    # plot the point
+    plt.style.use("classic")
+    fig, ax = plt.subplots()
+    point_numbers = range(rw.num_points)
+    ax.scatter(
+        rw.x_values,
+        rw.y_values,
+        c=point_numbers,
+        cmap=plt.cm.Blues,
+        edgecolors="none",
+        s=15,
+    )
+    plt.show()
 
-# plot the point
-plt.style.use("classic")
-fig, ax = plt.subplots()
-ax.scatter(rw.x_values, rw.y_values, s=15)
-plt.show()
+    keep_running = input("make another walk? (y/n)")
+    if keep_running == "n":
+        break
