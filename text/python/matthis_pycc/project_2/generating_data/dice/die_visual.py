94d70b987df349402bb3667bc4225cf6dd8cf727
James <119552925+BeallJames@users.noreply.github.com>
2024-03-21 22:30:48 -0700

    die update

--- text/python/matthis_pycc/project_2/generating_data/dice/die_visual.py
+++ text/python/matthis_pycc/project_2/generating_data/dice/die_visual.py
@@ -6,26 +6,29 @@ from die import Die
 
 
 # create a d6
-die = Die()
+die_1 = Die()
+die_2 = Die()
 
 # roll and store
 results = []
 for roll_num in range(1000):
-    results.append(die.roll())
+    result = die_1.roll() + die_2.roll()
+    results.append(result)
 
 # analyze results
 frequencies = []
-for value in range(1, die.num_sides + 1):
+max_result = die_1.num_sides + die_2.num_sides
+for value in range(2, max_result + 1):
     frequency = results.count(value)
     frequencies.append(frequency)
 
 # visualize results
-x_values = list(range(1, die.num_sides + 1))
+x_values = list(range(2, max_result + 1))
 data = [Bar(x=x_values, y=frequencies)]
 
-x_axis_config = {"title": "Result"}
+x_axis_config = {"title": "Result", "dtick": 1}
 y_axis_config = {"title": "Freq of Results"}
 my_layout = Layout(
-    title="results of d6 * 1000", xaxis=x_axis_config, yaxis=y_axis_config
+    title="results of 2xd6 * 1000", xaxis=x_axis_config, yaxis=y_axis_config
 )
-offline.plot({"data": data, "layout": my_layout}, filename="d6.html")
+offline.plot({"data": data, "layout": my_layout}, filename="2d6.html")
