1eefd57350adbb60e97459a3dbfb68c3fe657e4d
James <119552925+BeallJames@users.noreply.github.com>
2024-03-21 18:25:22 -0700

    Update scatter_squares.py

--- text/python/matthis_pycc/project_2/generating_data/scatter_squares.py
+++ text/python/matthis_pycc/project_2/generating_data/scatter_squares.py
@@ -1,13 +1,19 @@
 import matplotlib.pyplot as plt
 
+x_values = range(1, 1001)
+y_values = [x**2 for x in x_values]
+
 plt.style.use("seaborn-v0_8-darkgrid")
 fig, ax = plt.subplots()
-ax.scatter(2, 4, s=200)
+ax.scatter(x_values, y_values, s=10)
 
 # title and axes
 ax.set_title("square numbers", fontsize=24)
 ax.set_xlabel("value", fontsize=14)
 ax.set_ylabel("squared value", fontsize=14)
+
+# set range for axes
+ax.axis([0, 1100, 0, 1100000])
 
 # tick
 ax.tick_params(axis="both", which="major", labelsize=14)
