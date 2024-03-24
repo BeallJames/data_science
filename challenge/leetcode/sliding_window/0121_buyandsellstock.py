b1c251ad05e55ff3fac3305ae63353385f2eaade
James <119552925+BeallJames@users.noreply.github.com>
2024-03-23 18:47:19 -0700

    sliding window

--- challenge/leetcode/sliding_window/0121_buyandsellstock.py
+++ challenge/leetcode/sliding_window/0121_buyandsellstock.py
@@ -3,11 +3,22 @@ from typing import List
 
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        return len(prices)
+        if len(prices) < 2:
+            return 0
+
+        l, r = 0, 1
+        maxprofit = max(0, prices[r] - prices[l])
+        for i in range(1, len(prices)):
+            if prices[i] < prices[l]:
+                l = i
+                r = i
+            elif prices[i] > prices[r]:
+                r = i
+            if prices[r] - prices[l] > maxprofit:
+                maxprofit = prices[r] - prices[l]
+        return maxprofit
 
 
-prices = [7, 1, 5, 3, 6, 4]
-# output = 5
-
+prices = [7, 6, 4, 3, 1]
 sol = Solution()
-print(sol.maxProfit(prices))
+print(sol.maxProfit(prices))  # output: 5
