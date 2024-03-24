0604b78e1875e49c1aca998626228a47bb04c243
James <119552925+BeallJames@users.noreply.github.com>
2024-03-24 01:10:15 -0700

    Update 0424_longestrepeatingcharreplace.py

--- challenge/leetcode/sliding_window/0424_longestrepeatingcharreplace.py
+++ challenge/leetcode/sliding_window/0424_longestrepeatingcharreplace.py
@@ -38,3 +38,22 @@ print(sol.characterReplacement(s, k))
 
 #             ans = right - left + 1
 #         return ans
+
+
+# class Solution:
+#     def characterReplacement(self, s: str, k: int) -> int:
+
+#         l = 0
+#         d = {}
+#         maxi = 0
+
+#         for r in range(len(s)):
+
+#             d[s[r]] = 1+d.get(s[r], 0)
+#             maxi = max(maxi, d[s[r]])
+
+#             while (r-l+1)-maxi>k:
+#                 d[s[l]]-=1
+#                 l+=1
+
+#         return (r-l+1)
