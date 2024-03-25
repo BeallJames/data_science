691b06a601f78457ad5d9208b03073439c747f0d
James <119552925+BeallJames@users.noreply.github.com>
2024-03-24 21:01:45 -0700

    Update models.py

--- text/python/matthis_pycc/project_3/learning_log/learning_logs/models.py
+++ text/python/matthis_pycc/project_3/learning_log/learning_logs/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Topic(models.Model):
+    """a topic the user is learning about"""
+
+    text = models.CharField(max_length=200)
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        """return str representation of model"""
+        return self.text
